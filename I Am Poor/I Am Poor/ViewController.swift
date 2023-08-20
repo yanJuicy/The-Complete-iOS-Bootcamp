0a583f7a468514a33a2ede9c99c0f34bddb52c83
yanJuicy <hanjub3@gmail.com>
2023-08-20 21:19:43 +0900

    I Am Poor

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by lyanju on 2023/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
